ea54d4d69dbdcaa189197fa9c2bac3700655c087
Carsten-Christian Platz <mail@ccplatz.de>
2023-06-06 17:54:39 +0200

    final optimizations

--- 20230125_Lichtverfolger.ino
+++ 20230125_Lichtverfolger.ino
@@ -1,83 +1,87 @@
 // C++ code
-//
 #include <Servo.h>
 #include <LiquidCrystal.h>
 
 Servo servo_13;
 LiquidCrystal lcd_1(3, 4, 8, 9, 10, 11);
 
-int wait = 100;
+int wait = 200;
 
-int rawVoltage = 0;
+// solar paneel
+double rawVoltage = 0;
 int voltage = 0;
 int maxVoltage = 5;
+double outputVoltage = 0;
 
+// light detection
 int leftLightIntensity = 0;
 int rightLightIntensity = 0;
-int tolerance = 20;
-int angle = 90;
-int maxAngle = 179;
+int tolerance = 40;
+
+// turning tower
+int angle = 45;
+int maxAngle = 90;
 int minAngel = 0;
 
-// TODOS
-// Eingänge auf Variablen umschreiben
-// Kommentare auf Englisch
-
-
+#define SOLAR_PANEEL A0
+#define RIGHT_EYE A1
+#define LEFT_EYE A2
 
 void setup()
 {
-  // Set up the number of columns and rows on the LCD.
+  // setup the number of columns and rows on the LCD.
   lcd_1.begin(16, 2);
-  
+
+  // setup serial monitor
   Serial.begin(9600);
-  
+
+  // setup servo engine
   servo_13.attach(13, 500, 2500);
-  
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
+  servo_13.write(maxAngle / 2);
+
+  // setup analog pins
+  pinMode(SOLAR_PANEEL, INPUT);
+  pinMode(RIGHT_EYE, INPUT);
+  pinMode(LEFT_EYE, INPUT);
 }
 
 void loop()
 {
-  // Berechnung der Spannung des Photovoltaik Paneels
-  rawVoltage = analogRead(A0);
-  //Serial.print("Ausgangsspannung Paneel: ");
-  //Serial.print(rawVoltage);
-  //Serial.println(" V");
+  // Calculate voltage of solar paneel
+  rawVoltage = analogRead(SOLAR_PANEEL);
+  outputVoltage = (rawVoltage * 5 / 1024);
+  Serial.print("Ausgangsspannung Paneel: ");
+  Serial.println((String)outputVoltage + " Volt");
   
-  // Anzeige der Spannung auf dem LCD
+  // output voltage on LCD
   lcd_1.print("Spannung:");
   lcd_1.setCursor(0, 1);
-  lcd_1.print(rawVoltage);
+  lcd_1.print((String)outputVoltage + " Volt");
   lcd_1.setCursor(0, 0);
 
-  // Einlesen der Lichtstärke
-  leftLightIntensity = analogRead(A1);
-  rightLightIntensity = analogRead(A2);
+  // detect light intensity
+  leftLightIntensity = analogRead(RIGHT_EYE);
+  rightLightIntensity = analogRead(LEFT_EYE);
   Serial.print("Left eye: ");
   Serial.println(leftLightIntensity);
   Serial.print("Right eye: ");
   Serial.println(rightLightIntensity);
   
-  delay(1000); // ENTFERNEN!!!!!!!!!!!!!!!!!
-
-  // Drehung des Podests
+  // turn the tower
   if (rightLightIntensity > leftLightIntensity + tolerance) {
-    angle = (angle - 2);
+    angle = (angle + 2);
     if (angle < minAngel) {
       angle = minAngel;
     }
   }
   if (leftLightIntensity > rightLightIntensity + tolerance) {
-    angle = (angle + 2);
+    angle = (angle - 2);
     if (angle > maxAngle) {
       angle = maxAngle;
     }
   }
   servo_13.write(angle);
   
-  // Wait for some millisecond(s)
+  // wait for some millisecond(s)
   delay(wait); 
 }
